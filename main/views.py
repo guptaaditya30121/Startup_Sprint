5e3742ee65a209de28071e1800c64e9f415eff76
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-02-13 23:50:10 +0530

    added authentication

--- main/views.py
+++ main/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.conf import settings
+from .models import User
 
 # Create your views here.
 
 
 def login(request):
-    return HttpResponse("working")
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.filter(username=username).first()
+        if user:
+            user = authenticate(request, username=username, password=password)
+            if user is None:
+                messages.error(request, 'Incorrect Password')
+            else:
+                login(request, user)
+        else:
+            messages.error(request, "Username is not Correct")
+    return render(request, 'main/login.html')
 
 
 def register(request):
-    return HttpResponse("working")
+    if request.method == "POST":
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        User.objects.create_user(
+            username=username, email=email, password=password)
+        redirect('login')
+    return render(request, 'main/register.html')
 
 
+@login_required(login_url='login')
 def profile(request):
-    return HttpResponse("working")
+    user = request.user
+    return render(request, 'main/profile.html', {'user': user})
 
 
-def profile(request):
-    return HttpResponse("working")
+@login_required(login_url='login')
+def logout_view(request):
+    logout(request)
--- readme.md
+++ readme.md
@@ -1,14 +1,25 @@
 ## Setup
 
 Create Virtual Environment
+
 ```bash
   virtualenv venv
 ```
+
+Activate Virtual Enviroment
+
+```bash
+  venv/scripts/activate
+```
+
 Install Packages
+
 ```bash
   pip install -r "requirements.txt"
 ```
+
 Run the Server
+
 ```bash
-    python manage.py runserver
+  python manage.py runserver
 ```
