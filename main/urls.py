fad87c62cd17b75d31ddc1f9214bbcd5be3ac676
aditya <guptaaditya30121@gmail.com>
2023-02-16 16:44:54 +0530

    routes

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
 
 urlpatterns = [
     path('', views.login, name="login"),
@@ -7,5 +9,5 @@ urlpatterns = [
     path('profile/', views.profile, name="profile"),
     path('leaderboard/<int:id>/', views.leaderboard, name="leaderboard"),
     path('coupon/', views.coupon_page, name="coupon_page"),
-    path('logout/', views.logout, name="logout")
+    path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name="logout")
 ]
